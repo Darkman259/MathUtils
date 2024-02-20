e4583379d4d67a0ca87ebbe46eeb134dccf0cd1d
Darkman259 <88939160+Darkman259@users.noreply.github.com>
2024-02-21 03:48:49 +0530

    Update MathUtills.java

--- src/main/java/MathUtills.java
+++ src/main/java/MathUtills.java
@@ -1,1 +1,27 @@
+public class MathUtils {
 
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public double divide(int a, int b) {
+        if (b == 0) {
+            return -1.0; 
+        } else {
+            return (double) a / b;
+        }
+    }
+    public static void main(String[] args) {
+        MathUtils mathUtils = new MathUtils();
+        System.out.println("Addition: "+mathUtils.add(6, 3));
+        System.out.println("Subtraction: "+mathUtils.subtract(6, 3));
+        System.out.println("Multiplication: "+mathUtils.multiply(6, 3));
+        System.out.println("Division: "+mathUtils.divide(6, 3));
+        System.out.println("Division by zero: "+mathUtils.divide(6, 0));
+    }
+}
