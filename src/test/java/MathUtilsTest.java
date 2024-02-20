a5c5cf04c0ced12b4d1ebb503ce11578d9c48a96
Chandra mouli <88939160+Darkman259@users.noreply.github.com>
2024-02-21 04:37:34 +0530

    New

--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -1,54 +1,44 @@
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-public class MathUtilsTest
+public class MathUtilsTest {
 
-{
-private MathUtils mathUtils;
+    private MathUtils mathUtils;
 
-@Before 
- public void setUp ()
-  {
-mathUtils = new MathUtils (); 
-} 
- 
-@After 
- public void tearDown ()
-  {
-mathUtils = null;
-} 
- 
-@Test 
- public void testAdd ()
-  {
-Assert.assertEquals (5, mathUtils.add (2, 3));
-Assert.assertEquals (10, mathUtils.add (5, 5));
-Assert.assertEquals (-1, mathUtils.add (-1, 0));
-} 
- 
-@Test 
- public void testSubtract ()
-  {
-Assert.assertEquals (5, mathUtils.subtract (10, 5));
-Assert.assertEquals (-5, mathUtils.subtract (5, 10));
-Assert.assertEquals (0, mathUtils.subtract (5, 5));
-} 
+    @Before
+    public void setUp() {
+        mathUtils = new MathUtils();
+    }
 
-@Test 
- public void testMultiply ()
-  {
-Assert.assertEquals (10, mathUtils.multiply (2, 5));
-Assert.assertEquals (-15, mathUtils.multiply (3, -5));
-Assert.assertEquals (0, mathUtils.multiply (0, 5));
-} 
+    @After
+    public void tearDown() {
+        mathUtils = null;
+    }
 
-@Test 
- public void testDivide ()
-  {
-Assert.assertEquals (2.0, mathUtils.divide (10, 5), 0.001);
-Assert.assertEquals (-1.0, mathUtils.divide (10, 0), 0.001);
-Assert.assertEquals (0.0, mathUtils.divide (0, 5), 0.001);
-} 
-} 
+    @Test
+    public void testAdd() {
+        assertEquals(5, mathUtils.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(-1, mathUtils.subtract(2, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6, mathUtils.multiply(2, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2.0, mathUtils.divide(6, 3), 0.0001);
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertEquals(-1.0, mathUtils.divide(6, 0), 0.0001);
+    }
+}
